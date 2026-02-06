432e49ea9ac3c3d36e49af9b32ca3f8d0b828764
Abhinav Sinha <sinhaabhinav015@gmail.com>
2026-02-06 23:32:45 +0530

    added middleware and me route

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        const response = NextResponse.json({ message: "User logged out successfully", success: true });
+        response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
+        return response;
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
+    }
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,57 @@
+"use client"
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+import { useState } from "react";
+
+
 export default function Profile() {
+  const router = useRouter();
+  const [data, setData] = useState("");
+
+  const logout = () => {
+    try {
+      axios.get("/api/users/logout");
+      toast.success("Logout successful");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
+  const getUserDetails = async () => {
+    try {
+      const res = await axios.get('/api/users/me');
+      console.log(res.data);
+      setData(res.data.user._id);
+      toast.success("User details fetched");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-2xl font-bold">Profile</h1>
       <p className="mt-2 text-gray-400">
         Go to /profile/yourName to see a dynamic profile
       </p>
+      <hr/>
+      <h2 className="p-1 rounded bg-green-500">{data === 'nothing' ? "Nothing" : <Link href={`/profile/${data}`}>{data}
+            </Link>}</h2>
+      <button
+        onClick={logout}
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >Logout</button>
+
+        <button
+        onClick={getUserDetails}
+        className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >GetUser Details</button>
+      
+
     </div>
   );
 }
